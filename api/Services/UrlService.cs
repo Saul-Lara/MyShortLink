cae0ee718e036866dc8680937da8f89588cfa28f
Saul Lara <hl.saul.01@gmail.com>
2026-08-08 01:36:30 -0600

    refactor(api): add dictionary storage and logging to UrlService

--- api/Services/UrlService.cs
+++ api/Services/UrlService.cs
@@ -7,11 +7,27 @@ namespace api.Services
 {
     public class UrlService : IUrlService
     {
+        private readonly Dictionary<string, string> _urlDatabase = new();
+        private readonly ILogger<UrlService> _logger;
+
+        public UrlService(ILogger<UrlService> logger)
+        {
+            _logger = logger;
+        }
+
         public Task<Url> CreateShortenedUrl(Url urlData)
         {
             Snowflake snowflake = new Snowflake();
             urlData.Id = (ulong)snowflake.NextID();
             urlData.ShortCode = Base62.Encode(urlData.Id);
+
+            _urlDatabase.Add(urlData.ShortCode, urlData.OriginalUrl);
+
+            _logger.LogInformation(
+            "Created short code {ShortCode} for target URL {OriginalUrl}",
+            urlData.ShortCode,
+            urlData.OriginalUrl
+        );
             
             return Task.FromResult(urlData);
         }
