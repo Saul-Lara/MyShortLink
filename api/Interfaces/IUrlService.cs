728af6d76c3bfe9dbaf3248d76da0c482f40b97d
Saul Lara <hl.saul.01@gmail.com>
2026-08-11 00:09:32 -0600

    feat(api): add GetOriginalUrl to IUrlService

--- api/Interfaces/IUrlService.cs
+++ api/Interfaces/IUrlService.cs
@@ -5,5 +5,7 @@ namespace api.Interfaces
     public interface IUrlService
     {
         Task<Url> CreateShortenedUrl(Url urlData);
+
+        Task<string?> GetOriginalUrl(string shortCode);
     }
 }
